1bec5a756704c222043935a23119e6d1107740b0
annelieselovespugs <annelieseardizzone@Brynhildr.local>
2020-09-29 23:24:05 -0700

    Major optimizations
    -moved dictionary to front of the file as opposed to behind the encoded values to speed up the decoding process
    -MANY changes to both the decoding methods and encoding methods to account for this change
    -(seriously, there were a LOT of changes)
    -consolidated unnecessary variables to streamline the code as a whole

--- LZWTester.java
+++ LZWTester.java
@@ -4,7 +4,13 @@ public class LZWTester {
 	public static void main (String [] args) throws IOException
 	{
 		Encoder en = new Encoder ();
+
+		//System.out.println("Encoding...");
 		en.encode("input.txt");
+
+		//System.out.println("Decoding...");
 		en.decode();
+
+		//System.out.println("Testing has completed! :)");
 	}
 }
